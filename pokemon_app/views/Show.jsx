c0300cda3fe24de54f84d950764ca507b728aad2
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-08-08 10:16:13 -0400

    Show view shows req.params.id

---
+++ pokemon_app/views/Show.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+class Show extends React.Component {
+render(){
+    const pokemon = this.props.pokemon
+    return(
+        <div>
+            <p>whatever</p>
+        </div>
+    )
+}
+
+}
+
+
+
+module.exports = Show;
